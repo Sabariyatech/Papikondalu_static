4ccf77f51268a1ccac07dcb27799b5fa103f8943
Sabariyatech <team@sabariyatech.in>
2025-10-27 19:58:10 +0530

    application/ld+json

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import ContactClient from './ContactClient'
 
 export const metadata: Metadata = {
@@ -8,5 +9,47 @@ export const metadata: Metadata = {
 }
 
 export default function Contact() {
-  return <ContactClient />
+  const localBusinessSchema = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "name": "Bhadradri Papikondalu Tours",
+    "description": "Leading Papikondalu tour operator offering Bhadradri temple visits, East Godavari river cruises, and Maredumalli adventures with 24/7 expert assistance and personalized service.",
+    "url": "https://www.bhadradripapikondalu.com/contact/",
+    "telephone": "+91-9848323488",
+    "email": "info@bhadradripapikondalu.com",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "Bhadrachalam",
+      "addressLocality": "Bhadradri Kothagudem",
+      "addressRegion": "Telangana",
+      "postalCode": "507111",
+      "addressCountry": "IN"
+    },
+    "contactPoint": {
+      "@type": "ContactPoint",
+      "telephone": "+91-9848323488",
+      "contactType": "Customer Support",
+      "areaServed": "IN",
+      "availableLanguage": ["English", "Telugu"],
+      "hoursAvailable": "Mo-Su 06:00-22:00",
+      "contactOption": "HearingImpairedSupported"
+    },
+    "sameAs": [
+      "https://www.facebook.com/profile.php?id=61579935625167",
+      "https://twitter.com/BhadradriPapikon",
+      "https://www.instagram.com/aswinigodavari_travel/",
+      "https://www.youtube.com/@AswiniGodavari_travels"
+    ]
+  }
+
+  return (
+    <>
+      <Script
+        id="local-business-schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
+      />
+      <ContactClient />
+    </>
+  )
 }
